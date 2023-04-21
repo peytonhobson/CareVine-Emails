900463a12f1043b0d92e149365b9dd3637f646b9
peytonhobson <peyton.hobson1@gmail.com>
2023-04-21 14:12:30 -0600

    Added live preview

--- scripts/preview-email.js
+++ scripts/preview-email.js
@@ -1,29 +1,37 @@
-var http = require('http');
-var fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 const Handlebars = require('handlebars');
 const path = require('path');
 
+const jsonPath = path.join(__dirname, '..');
 
-
-const main = async () => {
-    const open= await import('open');
-    const PORT=8080; 
+const getHTML = () => {
     const templateName = process.argv[2];
 
-    const jsonPath = path.join(__dirname, '..');
     const html = fs.readFileSync(`${jsonPath}/Emails/${templateName}/${templateName}-email.html`, 'utf8');
     const data = JSON.parse(fs.readFileSync(`${jsonPath}/Emails/${templateName}/${templateName}-data.json`, 'utf8'));
 
     const template = Handlebars.compile(html);
-    const htmlToSend = template(data);
+    return template(data);
+}
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(htmlToSend);  
-        response.end();
-    }).listen(PORT);
-
+const openBrowser = async () => {
+    const open = await import('open')
     open.default('http://localhost:8080');
 }
 
-main()
+http.createServer(function(request, response) {  
+
+    const templateName = process.argv[2];
+
+    response.writeHeader(200, {"Content-Type": "text/html"});  
+    response.write(getHTML());  
+    response.end();
+
+    fs.watchFile(`${jsonPath}/Emails/${templateName}/${templateName}-email.html`, () => {
+        response.write(getHTML())
+    }); 
+
+}).listen('8080');
+
+openBrowser();
